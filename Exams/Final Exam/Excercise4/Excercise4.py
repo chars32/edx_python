37084aaead446d28c956f9c0caab20a78ce44f96
Chars García <chars24@gmail.com>
2016-03-11 11:50:18 -0600

    Finished Excercise4

--- Exams/Final Exam/Excercise4/Excercise4.py
+++ Exams/Final Exam/Excercise4/Excercise4.py
@@ -9,33 +9,44 @@ def my_final_grade_calculation(filename):
 	for item in data:
 		no_return_list.append(item.strip('\n'))
 
+	no_spaces_list =[]
+	for item in no_return_list:
+		no_spaces_list.append(item.replace(' ',''))
+
 	list_of_lists = []
-	for x in no_return_list:
+	for x in no_spaces_list:
 		list_of_lists.append(x.split(','))
 
 	#--------------------------------------------
 	dictionary = {}
-	for element in list_of_lists:
-		name = element[0]
-		avg_quizes = 0
-		avg_assignment = 0
 
-		for quiz in range(1, 7):
-			avg_quizes += int(element[quiz])
+	for element in list_of_lists: #start running elements of list of lists
+		sort_quizes = []
+		sort_assignments = []
 
-		for assignment in range(7, 11):
-			avg_assignment += int(element[assignment])
+		for quiz in range(1,7): #get the values of quizes sorted less to more
+			sort_quizes.append(int(element[quiz]))
+		sort_quizes.sort()
 
-		average_final = (avg_quizes*.25 + avg_assignment*.25 + int(element[11]) + int(element[12]))/4
+		for assignment in range(7,11): #get the values of assginments sorted less to more
+			sort_assignments.append(int(element[assignment]))
+		sort_assignments.sort()
 
-		if int(average_final) >= 60:
-			dictionary[name] = 'pass'
+		max_quizes = sort_quizes[2:] #split the sort_quizes from position 2 to end
+		max_assignments = sort_assignments[1:] #split the sort_assignment from position 1 to end
+		midterm = int(element[11])
+		final = int(element[12])
+
+		#get the average of each student
+		average = ((sum(max_quizes)/4)*.25) + ((sum(max_assignments)/3)*.25) + (midterm*.25) + (final*.25)
+		average = "{0:5.2f}".format(average)
+
+		if float(average) >= 60:
+			dictionary[element[0]] ="pass"
 		else:
-			dictionary[name] = 'fail'
+			dictionary[element[0]] ="fail"
 
 	return dictionary
 
 
-
-
-print(my_final_grade_calculation('archivo.txt'))
+print(my_final_grade_calculation('archivodos.txt'))
